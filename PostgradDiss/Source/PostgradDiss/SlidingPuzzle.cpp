adc55d320a3862b8da7c03aa61e6bca0b292b0d6
Cat Appleby <catappleby1@proton.me>
2025-05-30 12:13:16 +0100

    When touching object, sliding puzzle menu opens

--- PostgradDiss/Source/PostgradDiss/SlidingPuzzle.cpp
+++ PostgradDiss/Source/PostgradDiss/SlidingPuzzle.cpp
@@ -1,12 +1,39 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿#include "SlidingPuzzle.h"
 
-
-#include "SlidingPuzzle.h"
-
-SlidingPuzzle::SlidingPuzzle()
+void USlidingPuzzle::NativeConstruct()
 {
+    Super::NativeConstruct();
+    SetupBoard();
+
+    if (Tile_0) { Tile_0->OnClicked.AddDynamic(this, &USlidingPuzzle::OnTileClicked); }
+        
 }
 
-SlidingPuzzle::~SlidingPuzzle()
+void USlidingPuzzle::OnTileClicked()
 {
+
+
+}
+
+void USlidingPuzzle::SetupBoard()
+{
+    //Fill board
+    int n = 1;
+    for (int r = 0; r < 4; ++r)
+        for (int c = 0; c < 3; ++c)
+            Board[r][c] = (r == 3 && c == 3) ? 0 : n++;
+
+    EmptyPos = { 5, 3 };
+
+    for (int r = 0; r < 4; ++r)
+        for (int c = 0; c < 4; ++c)
+        {
+            int Index = Board[r][c];
+            UButton* Tile = Tiles[Index];
+            if (Tile)
+            {
+                TileGrid->AddChildToUniformGrid(Tile, r, c);
+                Tile->SetVisibility(Index == 0 ? ESlateVisibility::Hidden: ESlateVisibility::Visible);
+            }
+        }
 }
--- PostgradDiss/Source/PostgradDiss/SlidingPuzzle.h
+++ PostgradDiss/Source/PostgradDiss/SlidingPuzzle.h
@@ -1,15 +1,60 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+﻿// SlidingPuzzle.h
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
+#include "Components/UniformGridPanel.h"
+#include "SlidingPuzzle.generated.h"     
 
-/**
- * 
- */
-class POSTGRADDISS_API SlidingPuzzle
+
+UCLASS()                                 
+class POSTGRADDISS_API USlidingPuzzle : public UUserWidget
 {
-public:
-	SlidingPuzzle();
-	~SlidingPuzzle();
+    GENERATED_BODY()                   
+
+protected:
+    UPROPERTY(meta = (BindWidget))
+    UUniformGridPanel* TileGrid;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* Tile_0;
+    UPROPERTY(meta = (BindWidget))
+    UButton* Tile_1;
+    UPROPERTY(meta = (BindWidget))
+    UButton* Tile_2;
+    UPROPERTY(meta = (BindWidget))
+    UButton* Tile_3;
+    UPROPERTY(meta = (BindWidget))
+    UButton* Tile_4;
+    UPROPERTY(meta = (BindWidget))
+    UButton* Tile_5;
+    UPROPERTY(meta = (BindWidget))
+    UButton* Tile_6;
+    UPROPERTY(meta = (BindWidget))
+    UButton* Tile_7;
+    UPROPERTY(meta = (BindWidget))
+    UButton* Tile_8;
+    UPROPERTY(meta = (BindWidget))
+    UButton* Tile_9;
+    UPROPERTY(meta = (BindWidget))
+    UButton* Tile_10;
+    UPROPERTY(meta = (BindWidget))
+    UButton* Tile_11;
+    UPROPERTY(meta = (BindWidget))
+    UButton* Tile_12;
+    UPROPERTY(meta = (BindWidget))
+    UButton* Tile_13;
+    
+    
+    virtual void NativeConstruct() override; 
+
+    // Board variables
+    TArray<UButton*> Tiles;
+    int Board[4][4];
+    FIntPoint EmptyPos;
+
+    UFUNCTION()
+    void OnTileClicked();
+    void SetupBoard();
 };
